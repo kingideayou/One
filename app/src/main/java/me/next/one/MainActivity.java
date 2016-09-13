6384f54e69ea23ec0141e3ceeabe392d55c3d3b0
NeXT <kingideayou@qq.com>
2016-09-13 11:41:11 +0800

    style: 添加HomeFragment

--- app/src/main/java/me/next/one/MainActivity.java
+++ app/src/main/java/me/next/one/MainActivity.java
@@ -1,9 +1,6 @@
 package me.next.one;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import me.next.one.home.view.HomeFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,15 +22,16 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initDrawer(toolbar);
+        initHomeFragment();
+    }
 
+    private void initHomeFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_container, HomeFragment.newInstance()).commitAllowingStateLoss();
+    }
+
+    private void initDrawer(Toolbar toolbar) {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
             this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
